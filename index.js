91091d03031a75e7cb6de59b7d4c299cd1aa1ca5
tomtechstarter <149696023+tomtechstarter@users.noreply.github.com>
2023-11-16 22:20:28 +0100

    Add Cloudwatch and simplify development

--- index.js
+++ index.js
@@ -2,54 +2,52 @@ const express = require('express')
 const app = express()
 const port = 3000
 const AWS = require('aws-sdk');
+const winston = require('winston')
+const WinstonCloudWatch = require('winston-cloudwatch');
 
-AWS.config.update({ region: 'eu-central-1' })
+// Read env variables
+const { AWS_REGION, LOG_GROUP, LOG_STREAM, DYNAMODB_TABLE } = process.env
+AWS.config.update({ region: AWS_REGION })
 
+// Logger
+const logger = new winston.createLogger({
+    format: winston.format.json(),
+    transports: [
+        new (winston.transports.Console)({
+            timestamp: true,
+            colorize: true,
+        })
+    ]
+});
 
-const docClient = new AWS.DynamoDB.DocumentClient({ region: 'eu-central-1' });
-
-
-
-
-app.put('/newuser', (req, res) => {
-    const body = req.body
-    console.log("Body", body)
-
-    docClient.put({
-        TableName: 'test',
-        Item: {
-            id: "999",
-            user_id: 'first',
-            //timestamp is the primary key
-            timestamp: 3,
-            title: 'The Secret',
-            content: 'Book'
-        }
-    }, (err, data) => {
-        if (err) {
-            console.log(err);
-            res.send('Error Hello world')
-        } else {
-            console.log(data);
-            res.send('Hello World!')
-        }
-    });
-
+logger.add(new WinstonCloudWatch({
+    awsRegion: AWS_REGION,
+    cloudWatchLogs: new AWS.CloudWatchLogs(),
+    logGroupName: LOG_GROUP,
+    logStreamName: LOG_STREAM
+}));
+app.use((req, res, next) => {
+    logger.log('info', `Requesting ${req.method} ${req.originalUrl}`, { tags: 'http', additionalInfo: { body: req.body, headers: req.headers } });
+    next()
 })
 
+// Initialize DB Client
+const docClient = new AWS.DynamoDB.DocumentClient({ region: AWS_REGION });
+
+
+// GET Requests
 app.get('/profile', (req, res) => {
     const userId = req.query.id
     docClient.get({
-        TableName: 'test',
+        TableName: DYNAMODB_TABLE,
         Key: {
             'id': String(userId)
         }
     }, (err, data) => {
         if (err) {
-            console.log(err);
-            res.send('Error Hello world')
+            logger.error(err);
+            res.send(`Failed to fetch data with id ${userId}`)
         } else {
-            console.log(data);
             res.json(data)
         }
     });
@@ -59,7 +57,23 @@ app.get('/shopping', (req, res) => {
     res.json({ user: { name: "Max" } })
 })
 
+// PUT
+app.put('/newuser', (req, res) => {
+    const body = JSON.parse(req.body)
 
+    docClient.put({
+        TableName: DYNAMODB_TABLE,
+        Item: body
+    }, (err, data) => {
+        if (err) {
+            logger.error(err)
+            res.send('Failed to add Item')
+        } else {
+            res.json(body)
+        }
+    });
+
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
