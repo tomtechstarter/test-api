abca8b62e15f207eab1741620157e6bb4c768d87
tomtechstarter <149696023+tomtechstarter@users.noreply.github.com>
2024-05-08 13:40:02 +0200

    Add basic routes

--- index.js
+++ index.js
@@ -1,133 +1,21 @@
-const express = require('express')
-const app = express()
-const AWS = require('aws-sdk');
-const winston = require('winston')
-const WinstonCloudWatch = require('winston-cloudwatch');
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-const port = 5000
+const port = 5000;
 
 // Use for development
-app.use(cors())
-
-
-
-// Read env variables
-const { AWS_REGION, LOG_GROUP, LOG_STREAM, DYNAMODB_TABLE } = process.env
-AWS.config.update({ region: AWS_REGION })
-
-// Init CloudWatch for Metrics
-const cloudwatch = new AWS.CloudWatch();
-
-// Logger
-const logger = new winston.createLogger({
-    format: winston.format.json(),
-    transports: [
-        new (winston.transports.Console)({
-            timestamp: true,
-            colorize: true,
-        })
-    ]
-});
-
-logger.add(new WinstonCloudWatch({
-    awsRegion: AWS_REGION,
-    cloudWatchLogs: new AWS.CloudWatchLogs(),
-    logGroupName: LOG_GROUP,
-    logStreamName: LOG_STREAM
-}));
-app.use((req, res, next) => {
-    logger.log('info', `Requesting ${req.method} ${req.originalUrl}`, { tags: 'http', additionalInfo: { body: req.body, headers: req.headers } });
-    next()
-})
-
-// Initialize DB Client
-const docClient = new AWS.DynamoDB.DocumentClient({ region: AWS_REGION });
-
-// Helper Functions
-const publishNewUserMetric = (userId) => {
-    try {
-        const params = {
-            MetricData: [
-                {
-                    MetricName: 'NewUser',
-                    Dimensions: [
-                        {
-                            Name: 'UserId',
-                            Value: userId
-                        }
-                    ],
-                    Timestamp: new Date(),
-                    Unit: 'Count',
-                    Value: 1
-                },
-            ],
-            Namespace: 'UserNamespace' // Example namespace
-        };
-
-        cloudwatch.putMetricData(params, (err, data) => {
-            if (err) {
-                console.error('Error putting metric data:', err);
-            } else {
-                console.log('Successfully put metric data:', data);
-            }
-        });
-    } catch (e) {
-        logger.error('Failed to Publish CloudWatch Metrics', e)
-    }
-}
-
+app.use(cors());
 
 // GET Requests
-app.get('/profile', async (req, res) => {
-
-    try {
-        const userId = req.query?.id
-        const input = {
-            TableName: DYNAMODB_TABLE,
-            Key: {
-                'id': String(userId)
-            }
-        }
-        const data = await docClient.get(input).promise()
-
-        res.json(data)
-
-    } catch (e) {
-        logger.error('Failed to get Profile', e)
-        res.send(e)
-    }
-
-})
-
-app.get('/test', (req, res) => {
-    try {
-        res.json({ user: { name: "Moritz" } })
-    } catch (e) {
-        console.log(e)
-    }
-})
-
-// PUT
-app.put('/newuser', async (req, res) => {
-    try {
-        const body = JSON.parse(req.body)
-
-        const input = {
-            TableName: DYNAMODB_TABLE,
-            Item: body
-        }
-
-        const data = await docClient.put(input).promise();
-
-        
-        res.json(data)
-    } catch (e) {
-        logger.error(e)
-        res.send(e)
-    }
-})
+app.get("/test", (req, res) => {
+  try {
+    res.json({ user: { name: "Moritz" } });
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
